8f51e1684f06ca0544e24066663f84b22f0ec835
Maksym Herbin <maxim.gerbin@gmail.com>
2017-09-06 00:28:52 +0200

    minor, js anyway don`t care

--- romannumeralskataselenium/web/roman_converter.js
+++ romannumeralskataselenium/web/roman_converter.js
@@ -1,6 +1,6 @@
 var ROMAN_DIGITS = {"I":1, "V":5, "X":10, "L":50, "C":100, "D":500, "M":1000}
 
-function convertRomanToNumeric(romanNumeral, previous){
+function convertRomanToNumeric(romanNumeral){
    var numericValue = 0;
    var previous = 0;
    for(i = romanNumeral.length - 1; i >= 0; i--){
